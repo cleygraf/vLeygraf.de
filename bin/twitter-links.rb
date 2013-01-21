69f1a29d91b5d9151762bd8649c0445d1d5f2794
Christoph Leygraf <chris@leygraf.de>
2013-01-21 07:22:45 +0100

    twitter-links.rb updates

--- bin/twitter-links.rb
+++ bin/twitter-links.rb
@@ -5,7 +5,10 @@ require 'faster_csv'
 require 'unshorten'
 
 twitterlinks="#{File.expand_path File.dirname(__FILE__)}/../tmp/twitter-links.txt"
-unshorturls = Hash.new {}
+unshortencachefile="#{File.expand_path File.dirname(__FILE__)}/../tmp/unshortencache.txt"
+unshorturls = Hash[]
+
+unshorturls = File.open("#{unshortencachefile}", "rb") {|io| Marshal.load(io)}
 
 FasterCSV.foreach("#{twitterlinks}", :quote_char => '"', :col_sep =>',', :row_sep =>:auto) do |row|
 	if row[0] != "ID" then
@@ -17,9 +20,15 @@ FasterCSV.foreach("#{twitterlinks}", :quote_char => '"', :col_sep =>',', :row_se
 		if tweettext =~ /\A"*LINK:\s*(.*\S)\s*(https*:\/\/.*)\s*$/i
 			linktext = [$1]
 			linkshorturl = [$2]
-			linkurl = Unshorten["#{linkshorturl}"]
-			unshorturls["#{linkshorturl}"] = linkurl
+			if ! unshorturls["#{linkshorturl}"] then
+				linkurl = Unshorten["#{linkshorturl}"]
+				unshorturls["#{linkshorturl}"] = "#{linkurl}"
+			else
+				linkurl = unshorturls["#{linkshorturl}"]
+			end
 		end
-		#puts "#{tweetdate} #{linktext} -> \"#{linkurl}\""
+		puts "#{tweetdate} #{linktext} -> \"#{linkurl}\""
 	end 
- end
+end
+
+File.open("#{unshortencachefile}", "wb") {|io| Marshal.dump(unshorturls, io)}
