c25578f0ddcb43ea2c67f15638f9e057fe8386dd
U-HUBBLE\cleygraf <cleygraf@Win7.hubble.leyux.org>
2013-02-07 14:00:32 +0100

    bin/twitter-links.rb modified to use json for export/import of data

--- bin/twitter-links.rb
+++ bin/twitter-links.rb
@@ -1,34 +1,46 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
-require 'faster_csv'
+require 'csv'
 require 'unshorten'
+require 'json'
 
 twitterlinks="#{File.expand_path File.dirname(__FILE__)}/../tmp/twitter-links.txt"
 unshortencachefile="#{File.expand_path File.dirname(__FILE__)}/../tmp/unshortencache.txt"
+unshortenjsonfile="#{File.expand_path File.dirname(__FILE__)}/../tmp/unshortencache.json"
 unshorturls = Hash[]
+tweets = Hash[]
 
-unshorturls = File.open("#{unshortencachefile}", "rb") {|io| Marshal.load(io)}
+#unshorturls = File.open("#{unshortencachefile}", "rb") {|io| Marshal.load(io)}
+unshorturls = JSON.parse(File.read("#{unshortenjsonfile}"))
 
-FasterCSV.foreach("#{twitterlinks}", :quote_char => '"', :col_sep =>',', :row_sep =>:auto) do |row|
+CSV.foreach("#{twitterlinks}", :quote_char => '"', :col_sep =>',', :row_sep =>:auto) do |row|
 	if row[0] != "ID" then
+		tweetid = row[0]
 		timestamp = row[1]
 		tweettext = row[3]
 		if timestamp =~ /\A(\d\d\d\d-\d\d-\d\d)/ then
 			tweetdate = [$1]
 		end
 		if tweettext =~ /\A"*LINK:\s*(.*\S)\s*(https*:\/\/.*)\s*$/i
-			linktext = [$1]
-			linkshorturl = [$2]
+			linktext = $1
+			linkshorturl = $2
 			if ! unshorturls["#{linkshorturl}"] then
 				linkurl = Unshorten["#{linkshorturl}"]
-				unshorturls["#{linkshorturl}"] = "#{linkurl}"
+				if linkurl != linkshorturl then
+					unshorturls["#{linkshorturl}"] = "#{linkurl}"
+				else
+					puts "Failed to unshorten \"#{linkshorturl}\" -> \"#{linkurl}\"" 
+				end
 			else
 				linkurl = unshorturls["#{linkshorturl}"]
 			end
 		end
-		puts "#{tweetdate} #{linktext} -> \"#{linkurl}\""
+		tweets["#{tweetid}"] = Hash[]
+		tweets["#{tweetid}"]["URL"] = "#{linkurl}"
+#		puts "#{tweetdate} #{linktext} -> \"#{linkurl}\" (\"#{linkshorturl}\")"
 	end 
 end
 
-File.open("#{unshortencachefile}", "wb") {|io| Marshal.dump(unshorturls, io)}
+#File.open("#{unshortencachefile}", "wb") {|io| Marshal.dump(unshorturls, io)}
+File.open("#{unshortenjsonfile}", 'w') { |io| io.puts unshorturls.to_json }
